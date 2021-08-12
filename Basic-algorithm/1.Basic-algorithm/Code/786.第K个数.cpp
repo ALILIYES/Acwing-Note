8afdf2c851249414aecc9e9886a26f64bbb05214
liliyang <1357918777@qq.com>
2021-08-12 09:09:50 +0800

    789.第k个数更改

--- Basic-algorithm/1.Basic-algorithm/Code/786.第K个数.cpp
+++ Basic-algorithm/1.Basic-algorithm/Code/786.第K个数.cpp
@@ -8,15 +8,8 @@ int knth_element(int q[],int l,int r,int k){
     while(i<j){
         do ++i;while(q[i]<x);
         do --j;while(q[j]>x);
-        if(i<j){
-            swap(q[i],q[j]);
-            printf("交换%d %d i:%d j:%d\n",q[i],q[j],i,j);
-        }
+        if(i<j)swap(q[i],q[j]);
     }
-    for(int i=0;i<5;++i){
-        printf("%d ",q[i]);
-    }
-    printf("j:%d l:%d r:%d x:%d\n",j,l,r,x);
     if (j - l + 1 >= k) return knth_element(q, l, j, k);
     else return knth_element(q, j + 1, r, k - (j - l + 1));
 }
